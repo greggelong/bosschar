f6683889bd5ba54625a9cc455e911336f439dddf
Greg K <gkreisman_cep@qq.com>
2025-02-20 15:46:04 +0800

    make full screen

---
+++ sketch1.js
@@ -0,0 +1,93 @@
+let img;
+let vscale;
+let cchrs = ["注", "意", "艺", "术", "工", "人", "们"];
+let mic;
+let micbutt;
+let cnv;
+
+function preload() {
+  img = loadImage("workerClr.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // cnv = createCanvas(windowWidth, windowHeight);
+  // //cnv.style("z-index", "1"); // Moves it on top of the background
+  // cnv.position(0, 0); // Ensures it covers the entire screen
+  // cnv.style("z-index", "1000"); // Moves canvas to the top layer
+  // cnv.style("position", "absolute"); // Prevents default static positioning
+  // cnv.style("top", "0");
+  // cnv.style("left", "0");
+  //let cx = (windowWidth - cnv.width) / 2;
+  //let cy = (windowHeight - cnv.height) / 2;
+  //cnv.position(cx, cy);
+  //vscale = width / 40;
+  if (width < height) {
+    vscale = width / 35; // vScale tied to window width so it can work on phone and computer
+    console.log("by width");
+  } else {
+    vscale = height / 35;
+    console.log("by height");
+  }
+  pixelDensity(1);
+  img.resize(width / vscale, height / vscale); //
+  print(vscale, width / vscale, img.width);
+  frameRate(10);
+  // Set up microphone input
+  // Set up microphone input
+  mic = new p5.AudioIn();
+  mic.start(); // Start capturing audio
+  micbutt = createButton("Press to Start Mic");
+  micbutt.style("font-size", "48px");
+  micbutt.style("background-color", "yellow");
+
+  micbutt.position(width / 2, height / 2);
+  micbutt.mouseClicked(unlockAudioContext);
+  textAlign(LEFT, TOP);
+}
+
+function draw() {
+  background(40);
+
+  //noStroke()
+
+  // loop through the small image
+  for (let y = 0; y < img.height; y++) {
+    // for each y there are some x's
+    for (let x = 0; x < img.width; x++) {
+      let iclr = img.get(x, y);
+      //print(iclr);
+      noStroke();
+      if (iclr[0] > 127 && iclr[1] < 60) {
+        // Get the current volume level from the microphone
+        let vol = mic.getLevel();
+
+        // Map the volume to n between 1 and 5 (adjust these numbers to suit your needs)
+        n = map(vol, 0, 0.1, 0, 255); // Adjust max volume threshold as needed
+
+        fill(n, 0, 0);
+      } else {
+        fill(iclr);
+      }
+
+      //rect(x*vscale,y*vscale,vscale+1,vscale+1)
+      textSize(vscale);
+      text(random(cchrs), x * vscale, y * vscale);
+    }
+  }
+  //noLoop();
+}
+function unlockAudioContext() {
+  micbutt.hide();
+  const audioCtx = getAudioContext();
+  if (audioCtx.state === "suspended") {
+    audioCtx
+      .resume()
+      .then(() => {
+        console.log("Audio context unlocked");
+      })
+      .catch((err) => {
+        console.error("Failed to unlock audio context:", err);
+      });
+  }
+}
